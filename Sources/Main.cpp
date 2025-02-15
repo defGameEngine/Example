15f43c458e94948180dbbfc77d5b742e92532b44
defini7 <alexander.suhov@hotmail.com>
2025-02-15 12:48:41 +0300

    Ready. Set. Go!

---
+++ Sources/Main.cpp
@@ -0,0 +1,36 @@
+#include "defGameEngine.hpp"
+#include <iostream>
+
+class Example : public def::GameEngine
+{
+public:
+    Example()
+    {
+        UseOnlyTextures(true);
+        SetTitle("Example");
+    }
+
+protected:
+    bool OnUserCreate() override
+    {
+        return true;
+    }
+
+    bool OnUserUpdate(float deltaTime) override
+    {
+        ClearTexture(def::BLACK);
+        DrawTextureString({ 50, 50 }, "Hello, defGameEngine!", def::BLUE);
+        
+        return true;
+    }
+};
+
+int main()
+{
+    Example test;
+
+    if (test.Construct(256, 240, 4, 4))
+        test.Run();
+
+    return 0;
+}
